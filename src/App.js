060f9edfe29fc5d4b2b564980fb0a8ddd48cd75d
Parth Panchal <20091596+parthpanchal123@users.noreply.github.com>
2020-12-22 15:02:59 +0530

    Basic components and styles setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import styled from "styled-components";
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const EditorContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700px;
+  font-famiy: "Lato", sans-serif;
+  margin-bottom: 1em;
+`;
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <AppContainer>
+      <Title>Hello</Title>
+      <EditorContainer />
+    </AppContainer>
+  );
 }
 
 export default App;
